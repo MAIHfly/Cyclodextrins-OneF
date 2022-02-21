e6b21a8931d491b767f6c8d7a92cc34dcae1fa49
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-21 16:58:14 -0500

    add to the number of cores

--- OneFile.py
+++ OneFile.py
@@ -18,14 +18,14 @@ MoI2.optimise(method=xtb)
 MoI1.optimise(method=orca)
 MoI2.optimise(method=orca)
 
-CoI1 = ade.Calculation(name=MoI1.name, molecule=MoI1, method=orca, keywords=orca.keywords.hess, n_cores=48)
-CoI2 = ade.Calculation(name=MoI2.name, molecule=MoI2, method=orca, keywords=orca.keywords.hess, n_cores=48)
+CoI1 = ade.Calculation(name=MoI1.name, molecule=MoI1, method=orca, keywords=orca.keywords.hess, n_cores=128)
+CoI2 = ade.Calculation(name=MoI2.name, molecule=MoI2, method=orca, keywords=orca.keywords.hess, n_cores=128)
 
 CoI1.output.filename = MoI1.name+'.out'
 CoI2.output.filename = MoI2.name+'.out'
 
-MoI1.calc_thermo(calc=CoI1, n_cores=48)
-MoI2.calc_thermo(calc=CoI2, n_cores=48)
+MoI1.calc_thermo(calc=CoI1, n_cores=128)
+MoI2.calc_thermo(calc=CoI2, n_cores=128)
 
 GibbsE1 = MoI1.free_energy
 GibbsE2 = MoI2.free_energy
