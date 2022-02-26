e804010938c02ade9b749042cd6290bf2e662581
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-26 14:06:57 -0500

    adjust print statements and some other stuff

--- OneFile.py
+++ OneFile.py
@@ -22,42 +22,58 @@ MoI2.optimise(method=xtb)
 R1.optimise(method=xtb)
 P1.optimise(method=xtb)
 
+print('molecules have been optimised with XTB')
+
 MoI1.optimise(method=orca)
 MoI2.optimise(method=orca)
 R1.optimise(method=orca)
 P1.optimise(method=orca)
+
+print('molecules have been optimised with ORCA')
 
 CoI1 = ade.Calculation(name=MoI1.name, molecule=MoI1, method=orca, keywords=orca.keywords.hess, n_cores=48)
 CoI2 = ade.Calculation(name=MoI2.name, molecule=MoI2, method=orca, keywords=orca.keywords.hess, n_cores=48)
 CoIR1 = ade.Calculation(name=R1.name, molecule=R1, method=orca, keywords=orca.keywords.hess, n_cores=48)
 CoIP1 = ade.Calculation(name=P1.name, molecule=P1, method=orca, keywords=orca.keywords.hess, n_cores=48)
 
+print('Calculations have been carried out')
+
 CoI1.output.filename = MoI1.name+'.out'
 CoI2.output.filename = MoI2.name+'.out'
 CoIR1.output.filename = R1.name+'.out'
 CoIP1.output.filename = P1.name+'.out'
 
-MoI1.calc_thermo(calc=CoI1, n_cores=128)
-MoI2.calc_thermo(calc=CoI2, n_cores=128)
-R1.calc_thermo(calc=CoIR1, n_cores=128)
-P1.calc_thermo(calc=CoIP1, n_cores=128)
+print('calculations have been output to files')
+
+MoI1.calc_thermo(calc=CoI1, n_cores=48)
+MoI2.calc_thermo(calc=CoI2, n_cores=48)
+R1.calc_thermo(calc=CoIR1, n_cores=48)
+P1.calc_thermo(calc=CoIP1, n_cores=48)
+
+print('thermodynamic calculations have been carried out')
 
 GibbsE1 = MoI1.free_energy
 GibbsE2 = MoI2.free_energy
 GibbsR1 = R1.free_energy
 GibbsP1 = P1.free_energy
 
+print('gibbs free energies acquired')
+
 print(f'gibbsE {MoI1.name} = {GibbsE1} Ha')
 print(f'gibbsE {MoI2.name} = {GibbsE2} Ha')
 print(f'gibbsE {R1.name} = {GibbsR1} Ha')
 print(f'gibbsE {P1.name} = {GibbsP1} Ha')
 
+print('gibbs free energies printed')
+
 DelGibbs = (GibbsR1 + GibbsE1) - (GibbsE2 + GibbsP1)
 
-print(f'delG = {DelGibbs} Ha for'+MoI1.name)
+print('Gibbs Delta has been Acquired')
 
-NameofFile = MoI1.name+".txt"
-String = f'delG = {DelGibbs} Ha for'+MoI1.name
+print(f'delG = {DelGibbs} Ha for {MoI1.name} reaction')
+
+NameofFile = f'{MoI1.name}.txt'
+String = f'delG = {DelGibbs} Ha for {MoI1.name}'
 
 with open(NameofFile, 'w') as f:
     f.write(String)
