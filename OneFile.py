03658146f36eb474c73583366d964c7f79cf2b85
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-27 19:44:58 -0500

    remove keywords from index and place directly in function

--- OneFile.py
+++ OneFile.py
@@ -5,14 +5,6 @@ import os
 Cores = 48
 ade.Config.n_cores = Cores
 
-kwds = ade.Config.ORCA.keywords
-kwds.sp = ['HF-3c']
-kwds.opt = ['HF-3c']
-kwds.low_opt = ['HF-3c']
-kwds.hess = ['HF-3c']
-kwds.grad = ['HF-3c']
-kwds.opt_ts = ['HF-3c']
-
 orca = ade.methods.ORCA()
 xtb = ade.methods.XTB()
 
@@ -40,10 +32,10 @@ P1.optimise(method=orca, n_cores=Cores)
 
 print('molecules have been optimised with ORCA', flush=True)
 
-CoI1 = ade.Calculation(name=MoI1.name, molecule=MoI1, method=orca, keywords=kwds.hess, n_cores=Cores)
-CoI2 = ade.Calculation(name=MoI2.name, molecule=MoI2, method=orca, keywords=kwds.hess, n_cores=Cores)
-CoIR1 = ade.Calculation(name=R1.name, molecule=R1, method=orca, keywords=kwds.hess, n_cores=Cores)
-CoIP1 = ade.Calculation(name=P1.name, molecule=P1, method=orca, keywords=kwds.hess, n_cores=Cores)
+CoI1 = ade.Calculation(name=MoI1.name, molecule=MoI1, method=orca, keywords=ade.HessianKeywords(['HF-3c']), n_cores=Cores)
+CoI2 = ade.Calculation(name=MoI2.name, molecule=MoI2, method=orca, keywords=ade.HessianKeywords(['HF-3c']), n_cores=Cores)
+CoIR1 = ade.Calculation(name=R1.name, molecule=R1, method=orca, keywords=ade.HessianKeywords(['HF-3c']), n_cores=Cores)
+CoIP1 = ade.Calculation(name=P1.name, molecule=P1, method=orca, keywords=ade.HessianKeywords(['HF-3c']), n_cores=Cores)
 
 print('Calculations have been carried out', flush=True)
 
@@ -54,10 +46,10 @@ CoIP1.output.filename = P1.name+'_hess_orca.out'
 
 print('calculations have been output to files', flush=True)
 
-MoI1.calc_thermo(calc=CoI1, keywords=kwds.hess, n_cores=Cores)
-MoI2.calc_thermo(calc=CoI2, keywords=kwds.hess, n_cores=Cores)
-R1.calc_thermo(calc=CoIR1, keywords=kwds.hess, n_cores=Cores)
-P1.calc_thermo(calc=CoIP1, keywords=kwds.hess, n_cores=Cores)
+MoI1.calc_thermo(calc=CoI1, n_cores=Cores)
+MoI2.calc_thermo(calc=CoI2, n_cores=Cores)
+R1.calc_thermo(calc=CoIR1, n_cores=Cores)
+P1.calc_thermo(calc=CoIP1, n_cores=Cores)
 
 print('thermodynamic calculations have been carried out', flush=True)
 
